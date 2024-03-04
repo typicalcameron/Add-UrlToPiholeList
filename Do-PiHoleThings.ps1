65e5264c8b811a87ecd2cef08c0d62235f9fa69e
typicalcameron <53804604+typicalcameron@users.noreply.github.com>
2024-03-03 21:15:41 -0500

    Create Do-PiHoleThings.ps1
    Initial Commit.

---
+++ Do-PiHoleThings.ps1
@@ -0,0 +1,23 @@
+﻿function Add-UrlToPiholeList
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$piholeIP,
+        [Parameter(Mandatory=$true)]
+        [string]$url,
+        [Parameter(Mandatory=$true)]
+        [string]$apikey,
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("black","white")]
+        [string]$List
+    )
+    
+    $apiUrl = "http://$piholeIP/admin/api.php?list=$list&auth=$apikey&add=$url"
+
+    try {
+        Invoke-RestMethod -Uri $apiUrl -Method Post
+        Write-Host "URL has been successfully added to the Pi-hole $($list)list."
+    } catch {
+        Write-Host "Failed to add URL to the Pi-hole $($list)list. Error: $_"
+    }
+}
